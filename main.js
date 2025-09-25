20b24e01b08347f4d302e2f52e102653355eb2e4
mark simmons <marksimmons8415@yahoo.com>
2025-09-25 15:13:46 -0500

    update html,css, and js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("Hello, world!");
+// HTML Selectors
+const mainForm = document.getElementById("main-form");
+const outputDiv = document.getElementById("output");
+
+    // Get the varibles of inputs
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+
+// Event Listener
+mainForm.addEventListener('submit', function (event) {
+
+    // Prevent the action from the form to refresh the page
+    event.preventDefault();
+    
+
+
+
+    console.log(`First Name: ${firstNameInput.value}`);
+    console.log(`Last Name: ${lastNameInput.value}`);
+
+    // Get the output
+    outputDiv.innerHTML =
+    `<p>My first name is: ${firstNameInput.value}</p>
+    <p>My last name is: ${lastNameInput.value}</p>`
+
+
+
+
+});
+
